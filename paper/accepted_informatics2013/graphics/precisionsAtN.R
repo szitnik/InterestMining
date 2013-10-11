75634e24a71ffcbacca8fa9531323baf3f560954
slavkoz <slavko@zitnik.si>
2013-10-11 08:12:59 +0200

    New MaxSim results

--- paper/accepted_informatics2013/graphics/precisionsAtN.R
+++ paper/accepted_informatics2013/graphics/precisionsAtN.R
@@ -9,6 +9,7 @@ plot(As[,1], As[,2],
      ylab="Precision@N", 
      xlab="Cut-off value N", 
      ylim = c(0.0, 0.9),
+     xlim = c(5, 31),
      col = "red",
      type="l",
      xaxt = "n",
@@ -28,21 +29,21 @@ points(As[,1], As[,2], pch = pchs[1])
 text(As[,1] + xOffset, As[,2] + yOffset, As[,2], cex=0.8, pos=4, col="black")
 
 points(Bs[,1], Bs[,2], pch = pchs[2])
-text(Bs[,1] + xOffset, Bs[,2] + yOffset, Bs[,2], cex=0.8, pos=4, col="black")
+text(Bs[,1] , Bs[,2], Bs[,2], cex=0.8, pos=4, col="black")
 
 points(Cs[,1], Cs[,2], pch = pchs[3])
-text(Cs[,1] + xOffset, Cs[,2] + yOffset, Cs[,2], cex=0.8, pos=4, col="black")
+text(Cs[,1] + xOffset, Cs[,2] - yOffset, Cs[,2], cex=0.8, pos=4, col="black")
 
 dev.off()
 }
 
 
 #TYPE 1 - Questions - Categories
-As <- matrix(c(5, 10, 15, 20, 30, 0.29, 0.44, 0.55, 0.61, 0.8), 5, 2)
-Bs <- matrix(c(5, 10, 15, 20, 30, 0.29, 0.44, 0.55, 0.61, 0.8), 5, 2)
-Cs <- matrix(c(5, 10, 15, 20, 30, 0.19, 0.35, 0.41, 0.52, 0.74), 5, 2)
-title <- "Type 1 - Questions - Categories evidences"
-dataNames <- c("No weighting", "Weighting", "Maximum Similarity")
+As <- matrix(c(5, 10, 15, 20, 30, 0.24, 0.4, 0.58, 0.62, 0.7), 5, 2)
+Bs <- matrix(c(5, 10, 15, 20, 30, 0.29, 0.46, 0.59, 0.64, 0.74), 5, 2)
+Cs <- matrix(c(5, 10, 15, 20, 30, 0.29, 0.48, 0.59, 0.71, 0.8), 5, 2)
+title <- "Type 1 - Questions - CE&Cat 1,2,3 concepts"
+dataNames <- c("I&I-STSS", "LinSTSS", "P2Q")
 file <- "type1questions_PAtN.pdf"
 draw(As, Bs, Cs, title, dataNames, file)
 
@@ -51,15 +52,15 @@ As <- matrix(c(5, 10, 15, 20, 30, 0.33, 0.48, 0.59, 0.69, 0.87), 5, 2)
 Bs <- matrix(c(5, 10, 15, 20, 30, 0.11, 0.11, 0.11, 0.11, 0.11), 5, 2) #TODO!!!!!!!!!
 Cs <- matrix(c(5, 10, 15, 20, 30, 0.27, 0.39, 0.54, 0.63, 0.79), 5, 2)
 title <- "Type 2 - Answers - Categories evidences"
-dataNames <- c("No weighting", "Weighting", "Maximum Similarity")
+dataNames <- c("I&I-STSS", "LinSTSS", "P2Q")
 file <- "type2answers_PAtN.pdf"
 draw(As, Bs, Cs, title, dataNames, file)
 
 #TYPE 3 - Answers - Categories
-As <- matrix(c(5, 10, 15, 20, 30, 0.22, 0.41, 0.54, 0.66, 0.81), 5, 2)
-Bs <- matrix(c(5, 10, 15, 20, 30, 0.11, 0.11, 0.11, 0.11, 0.11), 5, 2) #TODO!!!!!!!!!
-Cs <- matrix(c(5, 10, 15, 20, 30, 0.2, 0.37, 0.45, 0.55, 0.76), 5, 2)
-title <- "Type 3 - Answers - Categories evidences"
-dataNames <- c("No weighting", "Weighting", "Maximum Similarity")
+As <- matrix(c(5, 10, 15, 20, 30, 0.23, 0.39, 0.5, 0.55, 0.64), 5, 2)
+Bs <- matrix(c(5, 10, 15, 20, 30, 0.33, 0.41, 0.51, 0.59, 0.65), 5, 2) #TODO!!!!!!!!!
+Cs <- matrix(c(5, 10, 15, 20, 30, 0.35, 0.46, 0.61, 0.66, 0.77), 5, 2)
+title <- "Type 3 - Answers - CE&Cat 1,2,3 concepts"
+dataNames <- c("I&I-STSS", "LinSTSS", "P2Q")
 file <- "type3answers_PAtN.pdf"
 draw(As, Bs, Cs, title, dataNames, file)
